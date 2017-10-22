321be1112c38b756e34cc3c53a860cc9f249bf3b
MatheusCode <matheuscolaresdasilva@gmail.com>
2017-10-22 12:53:01 -0200

    Inclusão da Busca do Cep Automático

---
+++ SistemaSac_TESTE_CEP/src/java/br/com/websac/bean/ClientesBean.java
@@ -0,0 +1,175 @@
+package br.com.websac.bean;
+
+import br.com.websac.dao.ClientesDao;
+import br.com.websac.dao.WebServiceCep;
+import br.com.websac.entity.Clientes;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import static org.primefaces.component.contextmenu.ContextMenu.PropertyKeys.event;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
+
+/**
+ *
+ * @author Matheus Colares
+ */
+@Named(value = "clientesBean")
+@SessionScoped
+public class ClientesBean implements Serializable {
+
+    private Clientes clientes = new Clientes();
+    private ClientesDao clientesdao = new ClientesDao();
+    private List<Clientes> listclientes;
+    
+    private String cep = null;
+    private String tipoLogradouro;
+    private String logradouro;
+    private String estado;
+    private String cidade;
+    private String bairro;
+     
+    public String adicionarCliente(){
+        clientesdao.addCliente(clientes);
+        return "index";
+    }
+    
+    public String removerCliente(Clientes c){
+        this.clientes = c;
+        clientesdao.removerCliente(this.clientes);
+               
+        return "cadastroCliente";
+    }
+    
+    public List listarClientes(){
+        listclientes = clientesdao.getList();
+        return this.listclientes;
+    }
+        
+    public String carregarCliente(Clientes c){
+        clientes = c;
+        return"editarCliente";
+    
+    }
+    
+    public String atualizarCliente(){
+        clientesdao.atualizarCliente(clientes);
+        return "cadastrarCliente";
+    }
+    
+    public void encontraCEP() {
+        
+        WebServiceCep cepWebService = WebServiceCep.searchCep(getCep()); 
+        
+        if (cepWebService.wasSuccessful()) {
+            setTipoLogradouro(cepWebService.getLogradouroType());
+            setLogradouro(cepWebService.getLogradouro());
+            setEstado(cepWebService.getUf());
+            setCidade(cepWebService.getCidade());
+            setBairro(cepWebService.getBairro());
+            
+            clientes.setCep(getCep());
+            clientes.setCidade(getCidade());
+            clientes.setEndereco(getLogradouro());
+            clientes.setUf(getEstado());
+            clientes.setBairro(getBairro());
+        } else {
+ 
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Servidor não está respondendo",
+                            "Servidor não está respondendo"));
+        }
+ 
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(String tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    
+    
+    public Clientes getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Clientes clientes) {
+        this.clientes = clientes;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.clientes);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ClientesBean other = (ClientesBean) obj;
+        if (!Objects.equals(this.clientes, other.clientes)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
---
+++ SistemaSac_TESTE_CEP/src/java/br/com/websac/entity/Origem.java
@@ -0,0 +1,65 @@
+package br.com.websac.entity;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Matheus Colares
+ */
+@Entity
+@Table(name="ORIGEM")
+public class Origem {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String descricao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Origem other = (Origem) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
